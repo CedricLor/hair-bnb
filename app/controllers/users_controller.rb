699ef70748dcb8732eb23d77917732c300fc335e
David Verbustel <david.verbustel@gmail.com>
2015-06-15 15:19:48 +0200

    added User model, seed, controller Users show edit create update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+users = User.create([
+  { first_name: 'John', last_name: 'Doe', email: 'john.doe@gmail.com', picture: 'beard1.jpeg', description: 'Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis.' },
+  { first_name: 'Will', last_name: 'Miths', email: 'will.miths@gmail.com', picture: 'beard2.jpeg', description: 'Aenean lacinia bibendum nulla sed consectetur.'},
+  { first_name: 'Tommy', last_name: 'Bollock', email: 'tommy.bollock@gmail.com', picture: 'beard3.jpeg', description: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.' },
+  { first_name: 'Kevin', last_name: 'Bridges', email: 'kevin.bridges@gmail.Com', picture: 'beard4.jpeg', description: 'Donec sed odio dui. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'}
+])
+
+#[{ first_name: '' }, { last_name: '' }, { email: '' }, { picture: '' }, { description: '' }]
