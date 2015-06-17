129e34ceb24dfad4fc2b6af89d17ba784486a733
David Verbustel <david.verbustel@gmail.com>
2015-06-17 15:48:15 +0200

    added user avatar with paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_many :accomodations, dependent: :destroy
+
+  has_attached_file :picture,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616093152) do
+ActiveRecord::Schema.define(version: 20150617134137) do
 
   create_table "accomodations", force: :cascade do |t|
     t.integer  "accomodates"
@@ -31,10 +31,13 @@ ActiveRecord::Schema.define(version: 20150616093152) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.text     "picture"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
 end
