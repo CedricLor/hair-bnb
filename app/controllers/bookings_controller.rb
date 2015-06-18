a49ad06c5406905db4bc6e78411db8ff9a54f27a
Cedric <cedric.lor@gmail.com>
2015-06-18 12:55:54 +0200

    booking working -- need to set the migration

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,18 @@
 class BookingsController < ApplicationController
+  before_action :set_user, only: [:create]
+  before_action :set_accomodation, only: [:create]
+
   def create
+    # @accomodation = @user.accomodations.build(accomodations_params)
+    # @accomodation.save
+    # redirect_to user_path(@user)
+    @booking = @accomodation.bookings.build(renter_booking_params)
+    @booking.save!
+
+    # current_account.bookings.create
+    # account = Account.new(params[:account_id])
+    # account.bookings.create
+
   end
 
   def new
@@ -18,5 +31,24 @@ class BookingsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def renter_booking_params
+    params.require(:booking).permit(:from, :to, :user_id)
+  end
+
+  def owner_booking_params
+    params.require(:booking).permit(:accepted)
+  end
+
+  # insecure methods - To be redrafted with Devise
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_accomodation
+    @accomodation = Accomodation.find(params[:accomodation_id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# ActiveRecord::Schema.define(version: 20150617134137) do
 ActiveRecord::Schema.define(version: 20150617142543) do
-
 
   create_table "accomodations", force: :cascade do |t|
     t.integer  "accomodates"
