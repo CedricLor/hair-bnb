949eba368f6b03a8c830c853be4a4df6cda64e06
Cedric <cedric.lor@gmail.com>
2015-06-19 09:47:47 +0200

    small amendments to incorporate devise navigation instead of nested navigation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,12 @@
 class BookingsController < ApplicationController
-  before_action :set_user, only: [:create]
+  before_action :set_user, only: [:create, :update]
   before_action :set_accomodation, only: [:create]
 
   def create
     @booking = @accomodation.bookings.build(renter_booking_params)
+    @booking.user_id = @user.id
     @booking.save
-    redirect_to user_accomodation_path(@user, @accomodation)
+    redirect_to accomodation_path(@accomodation)
     # current_account.bookings.create
     # account = Account.new(params[:account_id])
     # account.bookings.create
@@ -15,9 +16,13 @@ class BookingsController < ApplicationController
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update(accepted: true)
+    redirect_to user_path(@user)
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def destroy
@@ -41,7 +46,8 @@ class BookingsController < ApplicationController
 
   # insecure methods - To be redrafted with Devise
   def set_user
-    @user = User.find(params[:user_id])
+    @user = current_user
+    # @user = User.find(params[:user_id])
   end
 
   def set_accomodation
